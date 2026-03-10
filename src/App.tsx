6146bcec5100373f31c7a3a23361515d1402e384
Benjamin Manjolo <benjaminmanjolo39@gmail.com>
2026-03-10 11:35:49 +0200

    deleted some err causing files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Welcome from './components/Welcome';
 import ServiceCatalog from './components/ServiceCatalog';
 import Calendar from './components/Calendar';
@@ -9,8 +8,7 @@ import AdminLogin from './components/AdminLogin';
 import AdminDashboard from './components/AdminDashboard';
 import BarberInspo from './components/BarberInspo';
 import PaymentPage from './components/Paymentpage';
-import NeumorphismDemo from './components/Neumorphism';
-import Glassmorphism from './components/Glassmorphism';
+
 
 function App() {
   return (
@@ -23,8 +21,6 @@ function App() {
         <Route path="/admin" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/check" element={<NeumorphismDemo />} />
-        <Route path="/glass" element={<Glassmorphism/>} />
         <Route path="/barber-inspo" element={<BarberInspo />} />
       </Routes>
     </Router>
